aab90c9df36852d43b71ba9da8af176819cc6408
Camilla Barros <camillaerbarros@gmail.com>
2024-10-16 16:49:55 -0300

    utilizando fetch api

--- get-address.ts
+++ get-address.ts
@@ -1,12 +1,36 @@
-async function getAddress(cep: string){
+// import { error } from "console";
+
+// async function getAddress(cep: string){
+//     const url = `https://viacep.com.br/ws/${cep}/json/`;
+//     // console.log(url);
+
+//     try {
+//         const response = await fetch(url);
+//         if(!response.ok){
+//             throw new Error(`Response status: ${response.status}`);
+//         }
+
+//     }
+
+//     // console.log(response);
+
+//     const json = await response.json();
+//     console.log(json);
+// } catch (error){
+//     console.error(error.message);
+// }
+
+// getAddress("51270380");
+// console.log("restante do código...")
+
+async function getAddress(cep: string) {
     const url = `https://viacep.com.br/ws/${cep}/json/`;
-    console.log(url);
-
-    const response = await fetch(url);
-    console.log(response);
-
-    const json = await response.json();
-    console.log(json);
-}
-
-getAddress("51270380");
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Ocorreu um erro");
+    }
+  }
+  getAddress("51270380")
